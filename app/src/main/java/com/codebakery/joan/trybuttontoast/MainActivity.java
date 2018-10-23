8ddc48e1d295ba2d47eafbfa4d4f4ea93cd8c95f
codebaker <jungah.kim76@gmail.com>
2018-10-23 14:53:57 +0900

    토글버튼 추가

--- app/src/main/java/com/codebakery/joan/trybuttontoast/MainActivity.java
+++ app/src/main/java/com/codebakery/joan/trybuttontoast/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +14,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+/*
     public void onClickWidget(View view) {
         Button button = (Button) view;
         Toast.makeText(view.getContext(), button.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
+*/
+
+    public void onClickWidget(View view) {
+        int id = view.getId();
+        String text = null;
+        switch(id) {
+            case R.id.button1:
+                text = ((Button)view).getText().toString();
+                break;
+            case R.id.button2:
+                text = ((Button)view).getText().toString();
+                break;
+            case R.id.toggleButton:
+                text = (((ToggleButton)view).isChecked())
+                        ? ((ToggleButton)view).getTextOn().toString() : ((ToggleButton)view).getTextOff().toString();
+                break;
+                default: text = "Nothing";
+        }
+        Toast.makeText(view.getContext(), text, Toast.LENGTH_SHORT).show();
     }
 }
